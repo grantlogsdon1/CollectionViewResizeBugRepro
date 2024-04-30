293d15de6b1d9cfd24098da8a0f24a756b2f2367
Grant Logsdon <grant.logsdon@cgi.com>
2024-04-30 13:59:45 -0500

    Add project files.

---
+++ CollectionViewResizeBugRepro/CollectionViewResizeBugRepro/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CollectionViewResizeBugRepro
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        double defaultHeight = 100;
+        double newHeight = 150;
+        public List<int> TestCollection { get; set; } = [1, 1, 1];
+
+        [ObservableProperty]
+        double btnHeight;
+
+        public MainPageViewModel()
+        {
+            BtnHeight = defaultHeight;
+        }
+
+        [RelayCommand]
+        private void ChangeHeight()
+        {
+            if (BtnHeight == defaultHeight)
+            {
+                BtnHeight = newHeight;
+            }
+            else
+            {
+                BtnHeight = defaultHeight;
+            }
+        }
+    }
+}
---
+++ CollectionViewResizeBugRepro/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CollectionViewResizeBugRepro
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
